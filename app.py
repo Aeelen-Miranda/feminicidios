628eecc039af77e3981d6f251b397a251834bfdc
Aeelen Miranda <77294891+Aeelen@users.noreply.github.com>
2025-06-28 21:10:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -32,5 +32,5 @@ app.layout = dbc.Container([
 ], fluid=True)
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
 
